eed64aae2a235c8a0e3dae2422a9d8d26e2d98a2
MattHazard <mrm.mue@gmail.com>
2025-05-08 16:55:34 -0700

    Updated add_metadata_to_db.py to add more metadata

--- add_metadata_to_db.py
+++ add_metadata_to_db.py
@@ -6,6 +6,11 @@ import re
 from supabase import create_client, Client
 
 
+def get_first_key(d):
+    for key in d:
+        return key
+
+
 def add_metadata_to_db(metadata_file, url, key):
     # Supabase client
     supabase = create_client(url, key)
@@ -16,10 +21,26 @@ def add_metadata_to_db(metadata_file, url, key):
     for i, channel_pattern in enumerate(metadata['channelPatterns']):
         channel_patterns_dict[i] = channel_pattern
 
+    bbox = training_images[get_first_key(training_images)]['chunk_names'][get_first_key(training_images[get_first_key(training_images)]['chunk_names'])]['bbox']
+    cube_size = bbox[3] - bbox[0]
+    output_folder = metadata.pop('output_folder')
+
+    # Regex pattern to find /YYYY/M(M)/D(D)/...
+    match = re.search(r'(/\d+/\d{1,2}/\d{1,2}/)', output_folder)
+
+    if match:
+        split_index = match.start(1)
+        server_folder = output_folder[:split_index]
+        output_folder = output_folder[split_index:].lstrip('/')
+    else:
+        raise ValueError("Could not find date folder in path.")
+
     prepared_entry = {
         'software_version': metadata.pop('software_version'),
-        'output_folder': metadata.pop('output_folder'),
+        'output_folder': output_folder,
         'elapsed_sec': metadata.pop('elapsed_sec'),
+        'cube_size': cube_size,
+        'server_folder': server_folder,
         'metadata_json': metadata  # Rest of the data in logs will go to metadata
     }
 
@@ -39,9 +60,10 @@ def add_metadata_to_db(metadata_file, url, key):
             prepared_cubes_entry_copy['chunk_name'] = chunk_name
             prepared_cubes_entry_copy['chunk'] = curr_chunk_list[0]
             prepared_cubes_entry_copy['time'] = curr_chunk_list[1]
-            prepared_cubes_entry_copy['z'] = curr_chunk_list[2]
-            prepared_cubes_entry_copy['y'] = curr_chunk_list[3]
-            prepared_cubes_entry_copy['x'] = curr_chunk_list[4]
+            bbox = chunk_metadata.pop('bbox')
+            prepared_cubes_entry_copy['z_start'] = bbox[0]
+            prepared_cubes_entry_copy['y_start'] = bbox[1]
+            prepared_cubes_entry_copy['x_start'] = bbox[2]
             prepared_cubes_entry_copy['channel'] = curr_chunk_list[5]
             prepared_cubes_entry_copy['occupancy_ratio'] = chunk_metadata.pop('occ_ratio')
             prepared_cubes_entry_copy['metadata_json'] = chunk_metadata
