9694e774f7f537ea6cd08c1ddb742b2ca5c20f29
MattHazard <mrm.mue@gmail.com>
2025-07-25 13:05:04 -0700

    Added error checking to add_metadata_to_db.py

--- add_metadata_to_db.py
+++ add_metadata_to_db.py
@@ -29,6 +29,7 @@ def get_data_location(supabase, input_folder):
             return row['Data location']
     raise Exception(f"Data location: {input_folder} was not found in the Google Sheet!")
 
+
 def get_first_key(d):
     for key in d:
         return key
@@ -36,7 +37,9 @@ def get_first_key(d):
 
 def add_metadata_to_db(metadata_file, url, key):
     # Supabase client
-    supabase = create_client(url, key, options=SyncClientOptions(postgrest_client_timeout=600, storage_client_timeout=600, schema="public"))
+    supabase = create_client(url, key,
+                             options=SyncClientOptions(postgrest_client_timeout=600, storage_client_timeout=600,
+                                                       schema="public"))
     with open(metadata_file) as f:
         metadata = json.load(f)
     training_images = metadata.pop('training_images')
@@ -63,47 +66,55 @@ def add_metadata_to_db(metadata_file, url, key):
         'metadata_json': metadata  # Rest of the data in logs will go to metadata
     }
 
-    # Insert to prepared table
-    response = supabase.table('prepared').insert(prepared_entry).execute()
+    prepared_id = None
+    response = None
+    try:
+        # Insert to prepared table
+        response = supabase.table('prepared').insert(prepared_entry).execute()
 
-    prepared_id = response.data[0]['id']
+        prepared_id = response.data[0]['id']
 
-    prepared_tiles_entry_list = []
-    for training_image, training_image_dict in training_images.items():
-        prepared_tiles_entry = {'prepared_id': prepared_id,
-                                'tile_name': training_image}
-        prepared_tiles_entry_list.append(prepared_tiles_entry)
-    response = supabase.table('prepared_tiles').insert(prepared_tiles_entry_list).execute()
+        prepared_tiles_entry_list = []
+        for training_image, training_image_dict in training_images.items():
+            prepared_tiles_entry = {'prepared_id': prepared_id,
+                                    'tile_name': training_image}
+            prepared_tiles_entry_list.append(prepared_tiles_entry)
+        response = supabase.table('prepared_tiles').insert(prepared_tiles_entry_list).execute()
 
-    prepared_cubes_entry_list = []
-    delimiters = r"[./]"
-    for training_image, training_image_dict in training_images.items():
-        prepared_cubes_entry = {'prepared_id': prepared_id,
-                                'tile_name': training_image}
-        for chunk_name, chunk_metadata in training_image_dict['chunk_names'].items():
-            curr_chunk_list = re.split(delimiters, chunk_name)
-            curr_chunk_list.remove('c')
-            prepared_cubes_entry_copy = copy.deepcopy(prepared_cubes_entry)
-            prepared_cubes_entry_copy['chunk'] = curr_chunk_list[0]
-            prepared_cubes_entry_copy['time'] = curr_chunk_list[1]
-            bbox = chunk_metadata.pop('bbox')
-            prepared_cubes_entry_copy['z_start'] = bbox[0]
-            prepared_cubes_entry_copy['y_start'] = bbox[1]
-            prepared_cubes_entry_copy['x_start'] = bbox[2]
-            prepared_cubes_entry_copy['channel'] = curr_chunk_list[5]
-            prepared_cubes_entry_copy['occupancy_ratio'] = chunk_metadata.pop('occ_ratio')
-            if chunk_metadata:
-                prepared_cubes_entry_copy['metadata_json'] = chunk_metadata
-            prepared_cubes_entry_list.append(prepared_cubes_entry_copy)
+        prepared_cubes_entry_list = []
+        delimiters = r"[./]"
+        for training_image, training_image_dict in training_images.items():
+            prepared_cubes_entry = {'prepared_id': prepared_id,
+                                    'tile_name': training_image}
+            for chunk_name, chunk_metadata in training_image_dict['chunk_names'].items():
+                curr_chunk_list = re.split(delimiters, chunk_name)
+                curr_chunk_list.remove('c')
+                prepared_cubes_entry_copy = copy.deepcopy(prepared_cubes_entry)
+                prepared_cubes_entry_copy['chunk'] = curr_chunk_list[0]
+                prepared_cubes_entry_copy['time'] = curr_chunk_list[1]
+                bbox = chunk_metadata.pop('bbox')
+                prepared_cubes_entry_copy['z_start'] = bbox[0]
+                prepared_cubes_entry_copy['y_start'] = bbox[1]
+                prepared_cubes_entry_copy['x_start'] = bbox[2]
+                prepared_cubes_entry_copy['channel'] = curr_chunk_list[5]
+                prepared_cubes_entry_copy['occupancy_ratio'] = chunk_metadata.pop('occ_ratio')
+                if chunk_metadata:
+                    prepared_cubes_entry_copy['metadata_json'] = chunk_metadata
+                prepared_cubes_entry_list.append(prepared_cubes_entry_copy)
 
-    # Insert 10000 cube entries at a time if possible
-    insert_batch_size = 10000
-    num_cube_entries = len(prepared_cubes_entry_list)
-    insert_batch_size = min(insert_batch_size, num_cube_entries)
-    for i in range(0, num_cube_entries, insert_batch_size):
-        response = supabase.table('prepared_cubes').insert(prepared_cubes_entry_list[i:i+insert_batch_size]).execute()
+        # Insert 10000 cube entries at a time if possible
+        insert_batch_size = 10000
+        num_cube_entries = len(prepared_cubes_entry_list)
+        insert_batch_size = min(insert_batch_size, num_cube_entries)
+        for i in range(0, num_cube_entries, insert_batch_size):
+            response = supabase.table('prepared_cubes').insert(
+                prepared_cubes_entry_list[i:i + insert_batch_size]).execute()
 
-    #response = supabase.table('prepared_cubes').insert(prepared_cubes_entry_list).execute()
+        #response = supabase.table('prepared_cubes').insert(prepared_cubes_entry_list).execute()
+    except:
+        if prepared_id is not None:
+            supabase.table('prepared').delete().eq('id', prepared_id).execute()
+        raise Exception(f'Insertion failed. Response: {response}')
     return response
 
 
